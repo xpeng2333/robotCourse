bddafbcab30562ab04f9d46bb0c570843b531726
xpeng2333 <2544627510@qq.com>
2019-11-26 23:40:09 +0800

    homework1_part1

---
+++ h1/p1/example/useHello.c
@@ -0,0 +1,6 @@
+#include <hello.h>
+int main(int argc, char const *argv[])
+{
+    sayHello();
+    return 0;
+}
---
+++ h1/p1/src/hello.c
@@ -0,0 +1,6 @@
+#include "../include/hello.h"
+
+void sayHello()
+{
+    printf("Hello\n");
+}
